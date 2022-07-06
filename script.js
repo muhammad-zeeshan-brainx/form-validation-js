cbaa73b594c836b02ebe4752c00da4ed28bb662c
muhammad-zeeshan-brainx <muhammad.zeeshan@brainxtech.com>
2022-07-06 18:22:24 +0500

    refactoring code

--- script.js
+++ script.js
@@ -1,23 +1,140 @@
-submitBtn = document.querySelector("#submit-btn");
-// submitBtn.disabled = false;
-// submitBtn.classList.add("submit-btn");
+function validateForm() {
+  var formFields = document.getElementById("myForm").elements;
 
-//Validate password field
-function passwordValidator() {
-  //   alert("hey");
-  let password = document.querySelector("#myForm input[name='password']").value;
-  let confirmPassword = document.querySelector(
-    "#myForm input[name='confirmPassword']"
-  ).value;
+  let firstNameValidation = firstNameValidator(
+    formFields.namedItem("firstName").value
+  );
+  let lastNameValidation = lastNameValidator(
+    formFields.namedItem("lastName").value
+  );
+  let ageValidation = ageValidator(Number(formFields.namedItem("age").value));
+  let phoneValidation = phoneValidator(formFields.namedItem("phone").value);
+  let emailValidation = emailValidator(formFields.namedItem("email").value);
+  let passwordValidation = passwordValidator(
+    formFields.namedItem("password").value,
+    formFields.namedItem("confirmPassword").value
+  );
 
+  if (
+    firstNameValidation &&
+    lastNameValidation &&
+    ageValidation &&
+    phoneValidation &&
+    passwordValidation &&
+    emailValidation &&
+    passwordValidation
+  ) {
+    submitBtn = document.querySelector("#submit-btn");
+    submitBtn.disabled = false;
+    submitBtn.classList.add("submit-btn");
+  } else {
+    submitBtn = document.querySelector("#submit-btn");
+    submitBtn.disabled = true;
+    submitBtn.classList.remove("submit-btn");
+  }
+}
+
+//validate first name
+function firstNameValidator(firstName) {
+  let errorElement = document.querySelector(".first-name");
+
+  if (!firstName) {
+    errorElement.style.display = "block";
+    errorElement.innerHTML = "first name can not be blank";
+    return false;
+  } else if (!/[a-zA-Z]+$/.test(firstName)) {
+    errorElement.style.display = "block";
+    errorElement.innerHTML = "first name shoud only contain alphabets [A-z]";
+    return false;
+  } else {
+    errorElement.style.display = "none";
+    return true;
+  }
+}
+
+//validate lastname
+function lastNameValidator(lastName) {
+  let errorElement = document.querySelector(".last-name");
+  if (!lastName) {
+    errorElement.style.display = "block";
+    errorElement.innerHTML = "last name can not be blank";
+    return false;
+  } else if (!/[a-zA-Z]+$/.test(lastName)) {
+    errorElement.style.display = "block";
+    errorElement.innerHTML = "last name shoud only contain alphabets [A-z]";
+    return false;
+  } else {
+    console.log("display none");
+    errorElement.style.display = "none";
+    return true;
+  }
+}
+
+//validate age
+function ageValidator(age) {
+  let errorElement = document.querySelector(".age");
+  if (age - Math.floor(age) !== 0) {
+    errorElement.style.display = "block";
+    errorElement.innerHTML = "age must be a whole number";
+    return false;
+  } else if (!(age >= 18 && age <= 151)) {
+    errorElement.style.display = "block";
+    errorElement.innerHTML =
+      "age can not be blank and must be between 18 and 151";
+    return false;
+  } else {
+    errorElement.style.display = "none";
+    return true;
+  }
+}
+
+//Validate phone
+function phoneValidator(phone) {
+  let errorElement = document.querySelector(".phone");
+  if (phone.length != 11) {
+    errorElement.style.display = "block";
+    errorElement.innerHTML = "contact number must have 11 digits";
+    return false;
+  } else if (!/[0-9]/.test(Number(phone))) {
+    errorElement.style.display = "block";
+    errorElement.innerHTML = "contact number should only contain digits [0-9]";
+    return false;
+  } else {
+    errorElement.style.display = "none";
+    return true;
+  }
+}
+
+//validate email
+function emailValidator(email) {
+  let errorElement = document.querySelector(".email");
+  emailRegex = new RegExp("^[a-z]+[0-9]*@[a-z]+.(?=com$)|.(?=org$)|.(?=edu$)");
+  emailsArray = email.replace(/\s/g, "").split(",");
+
+  for (let i = 0; i < emailsArray.length; i++) {
+    if (!emailRegex.test(emailsArray[i])) {
+      errorElement.style.display = "block";
+      errorElement.innerHTML =
+        "email should be in the following format Example: abc@xyz.com";
+      return false;
+    } else {
+      errorElement.style.display = "none";
+      return true;
+    }
+  }
+}
+
+//validate password
+function passwordValidator(password, confirmPassword) {
+  let errorElement = document.querySelector(".password");
   if (password !== confirmPassword) {
-    console.log("if");
-    document.querySelector(".form-errors p").innerHTML =
-      "Password must match <br>";
+    errorElement.style.display = "block";
+    errorElement.innerHTML = "password must match";
+    return false;
   } else if (password.length < 8) {
-    console.log("else if");
-    document.querySelector(".form-errors p").innerHTML =
-      "<p>Minimum length is 8</p><br>";
+    errorElement.style.display = "block";
+    errorElement.innerHTML = "minimum password length is 8";
+    return false;
   } else if (
     !(
       /[A-Z]/.test(password) &&
@@ -25,86 +142,12 @@ function passwordValidator() {
       /[0-9]/.test(password)
     )
   ) {
-    document.querySelector(".form-errors p").innerHTML =
-      "Password should have both alphanumeric, at least one upper case and one the lower case letter<br>";
+    errorElement.style.display = "block";
+    errorElement.innerHTML =
+      "password should have both alphanumeric, at least one upper case and one the lower case letter";
+    return false;
   } else {
-    document.querySelector(".form-errors p").innerHTML = "";
-  }
-}
-
-//Validate Age field
-function ageValidator() {
-  let age = Number(document.querySelector("#myForm input[name='age']").value);
-  if (age - Math.floor(age) !== 0) {
-    document.querySelector(".form-errors p").innerHTML =
-      "Age must be a whole number<br>";
-  } else if (!(age >= 18 && age <= 151)) {
-    document.querySelector(".form-errors p").innerHTML =
-      "Age must be between 18 and 151<br>";
-  } else {
-    document.querySelector(".form-errors p").innerHTML = "";
-  }
-}
-
-//Validate phone field
-function phoneValidator() {
-  let phone = document.querySelector("#myForm input[name='phone']").value;
-  if (phone.length != 11) {
-    document.querySelector(".form-errors p").innerHTML =
-      "contact number must have 11 digits<br>";
-  } else if (!/[0-9]/.test(Number(phone))) {
-    document.querySelector(".form-errors p").innerHTML =
-      "contact number should only contain digits [0-9]<br>";
-  } else {
-    document.querySelector(".form-errors p").innerHTML = "";
-  }
-}
-
-function firstNameValidator() {
-  let firstName = document.querySelector(
-    "#myForm input[name='firstName']"
-  ).value;
-
-  if (!firstName) {
-    document.querySelector(".form-errors p").innerHTML =
-      "First name can not be blank<br>";
-  } else if (!/[a-zA-Z]+$/.test(firstName)) {
-    document.querySelector(".form-errors p").innerHTML =
-      "First name shoud only contain alphabets [A-z]<br>";
-  } else {
-    document.querySelector(".form-errors p").innerHTML = "";
-  }
-}
-
-function lastNameValidator() {
-  let lastName = document.querySelector("#myForm input[name='lastName']").value;
-
-  if (!lastName) {
-    document.querySelector(".form-errors p").innerHTML =
-      "Last name can not be blank<br>";
-  } else if (!/[a-zA-Z]+$/.test(lastName)) {
-    document.querySelector(".form-errors p").innerHTML =
-      "Last name shoud only contain alphabets [A-z]<br>";
-  } else {
-    document.querySelector(".form-errors p").innerHTML = "";
-  }
-}
-
-function emailValidator() {
-  let email = document.querySelector("#myForm input[name='email']").value;
-  emailRegex = new RegExp("^[a-z]+[0-9]*@[a-z]+.(?=com$)|.(?=org$)|.(?=edu$)");
-
-  emailsArray = email.replace(/\s/g, "").split(",");
-  console.log(emailsArray);
-  console.log(emailRegex.test(emailsArray[0]));
-
-  for (let i = 0; i < emailsArray.length; i++) {
-    if (!emailRegex.test(emailsArray[i])) {
-      document.querySelector(".form-errors p").innerHTML =
-        "email should be in the following format<br> Example: abc@xyz.com";
-      break;
-    } else {
-      document.querySelector(".form-errors p").innerHTML = "";
-    }
+    errorElement.style.display = "none";
+    return true;
   }
 }
